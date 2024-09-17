81a0ddd2a409721da883cd199964d4923a0697eb
rokugin <rokugin@gmail.com>
2024-09-17 11:14:25 -0700

    Convert to Harmony patches to avoid performing code in update loop, Remove unused code, Update version in manifest

---
+++ Patches/MineShaftPatch.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xna.Framework;
+using StardewValley;
+using StardewValley.Locations;
+using xTile.Tiles;
+
+namespace PassableDescents.Patches;
+
+static class MineShaftPatch {
+
+    public static void DoCreateLadder_Postfix(MineShaft __instance, Point point) {
+        DelayedAction delayedAction = new DelayedAction(200);
+        delayedAction.behavior = () => {
+            int X = point.X;
+            int Y = point.Y;
+            Tile tile = Game1.currentLocation.map.GetLayer("Buildings").Tiles[X, Y];
+
+            tile.Properties.Add("Passable", "T");
+        };
+        Game1.delayedActions.Add(delayedAction);
+    }
+
+    public static void DoCreateLadderAt_Postfix(Vector2 p) {
+        int X = (int)p.X;
+        int Y = (int)p.Y;
+        Tile tile = Game1.currentLocation.map.GetLayer("Buildings").Tiles[X, Y];
+
+        tile.Properties.Add("Passable", "T");
+    }
+
+}
