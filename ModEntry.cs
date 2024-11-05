4ac39e20f3ba9e43d35d2423496157aa9aab4756
rokugin <rokugin@gmail.com>
2024-11-05 09:21:14 -0700

    Update NuGet, Update version, Remove unused directives

--- ModEntry.cs
+++ ModEntry.cs
@@ -1,28 +1,24 @@
-﻿using Microsoft.Xna.Framework;
-using StardewModdingAPI;
-using StardewValley;
-using StardewValley.Extensions;
+﻿using StardewModdingAPI;
 using StardewValley.Locations;
-using xTile.Layers;
 using HarmonyLib;
 using PassableDescents.Patches;
 
-namespace PassableDescents {
-    internal class ModEntry : Mod {
+namespace PassableDescents;
 
-        public override void Entry(IModHelper helper) {
+internal class ModEntry : Mod {
 
-            var harmony = new Harmony(ModManifest.UniqueID);
+    public override void Entry(IModHelper helper) {
 
-            harmony.Patch(
-                original: AccessTools.Method(typeof(MineShaft), "doCreateLadderDown"),
-                postfix: new HarmonyMethod(typeof(MineShaftPatch), nameof(MineShaftPatch.DoCreateLadder_Postfix))
-            );
-            harmony.Patch(
-                original: AccessTools.Method(typeof(MineShaft), "doCreateLadderAt"),
-                postfix: new HarmonyMethod(typeof(MineShaftPatch), nameof(MineShaftPatch.DoCreateLadderAt_Postfix))
-            );
-        }
+        var harmony = new Harmony(ModManifest.UniqueID);
 
+        harmony.Patch(
+            original: AccessTools.Method(typeof(MineShaft), "doCreateLadderDown"),
+            postfix: new HarmonyMethod(typeof(MineShaftPatch), nameof(MineShaftPatch.DoCreateLadder_Postfix))
+        );
+        harmony.Patch(
+            original: AccessTools.Method(typeof(MineShaft), "doCreateLadderAt"),
+            postfix: new HarmonyMethod(typeof(MineShaftPatch), nameof(MineShaftPatch.DoCreateLadderAt_Postfix))
+        );
     }
+
 }
